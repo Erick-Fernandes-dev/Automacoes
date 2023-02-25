d58cc1b7052f56f679e625dca62526ae857dc108
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2023-02-25 10:32:23 -0300

    Automação versão 3

--- auto1/onu.py
+++ auto1/onu.py
@@ -1,6 +1,10 @@
 import xtelnet
+import pexpect
 import time
 
+print("="*52)
+print("="*15, "Sistema Automatizado", "="*15)
+print("="*52, "\n")
 
 t = xtelnet.session()
 
@@ -25,7 +29,7 @@ def onu_huawei():
 # 2
 def gm620():
 
-    opcao_de_versap = int(input("Digite a opção: (1 ou 2)"))
+    opcao_de_versap = int(input("Digite a opção: (1 ou 2): "))
 
 
     if (opcao_de_versap == 1):
@@ -49,28 +53,32 @@ def gm620():
         print("successfully reset...")
 
 # 3
-# def nokia():
+def nokia():
 
-#     connectTelnet('192.168.1.1', '', '', 23, 5)
-#     time.sleep(0.5)
-#     print("Processing...")
-#     nokiaComnad = t.execute("cfgcli -r")
-#     print(nokiaComnad)
+    ip = '192.168.1.1'
+    p = 23
 
-#     reboot = t.execute("reboot")
-#     print(reboot)
-#     print("successfully reset...")
+    tn = pexpect.spawn('telnet ' + ip)
 
-# if (onuType == 1):
-#     onu_huawei()
-# elif (onuType == 2):
-#     gm620()
+    #Comando que apaga tudo e retorna padrão de fábrica
+    tn.expect('/sbin # ')
+    tn.sendline('cfgcli -r')
+    tn.expect('/sbin # ')
+
+    print(tn.before.decode())#Imprime o comando que foi lançado anteriormente
+
+    # Reinicia o produto
+    tn.sendline('reboot')
+    tn.expect('/sbin # ')
+
+    print(tn.before.decode())
+    tn.close()
 
 boll = True
 
 while (boll):
 
-    option = str(input("Digite Sim para inicializar o sistema e Não para finalizar (s ou n): ")).lower()
+    option = str(input("Digite Sim (S) para inicializar o sistema e Não (N) para finalizar (s ou n): ")).lower()
 
     if (option == "s"):
 
@@ -78,8 +86,12 @@ while (boll):
 
         if (onuType == 1):
             onu_huawei()
+
         elif (onuType == 2):
             gm620()
+
+        elif (onuType == 3):
+            nokia()
 
         boll = True
     elif(option == "n"):
